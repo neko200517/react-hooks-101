7b9002a91c59282b0cb360902b89ed7fe7c8af46
pam190331 <pam130901@gmail.com>
2022-02-12 16:03:05 +0900

    implrement callbacks with useEffect.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = (props) => {
   const [state, setState] = useState(props);
   const { name, price } = state;
+
+  // 1. レンダリングの後に呼ばれる
+  // 2. 状態が変化したときに呼ばれる
+  useEffect(() => {
+    console.log('This is like componentDidMount or componentDidUpdate.');
+  });
+
+  // 1. 一度だけ呼ばれる
+  useEffect(() => {
+    console.log('This is like componentDitMount');
+  }, []);
+
+  // 1. 特定の状態が変更された場合に呼ばれる
+  useEffect(() => {
+    console.log('This callback is for name only.');
+  }, [name]);
 
   return (
     <>
