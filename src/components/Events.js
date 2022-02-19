508250b76a89cc0ff86ad9fc808bd05f15bd93a8
pam190331 <pam130901@gmail.com>
2022-02-19 23:02:41 +0900

    setup react context provider

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Event from './Event';
+import AppContext from '../contexts/AppContext';
 
 const Events = ({ state, dispatch }) => {
+  const value = useContext(AppContext);
   return (
     <>
       <label htmlFor='formEventBody'>イベント一覧</label>
