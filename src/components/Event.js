3807c4c6ac3b8183e7dd3bda2a9240b37d810cb3
pam190331 <pam130901@gmail.com>
2022-02-20 18:57:11 +0900

    create operationLogs

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import AppContext from '../contexts/AppContext';
-import { DELETE_EVENT } from '../actions';
+import { ADD_OPERATION_LOG, DELETE_EVENT } from '../actions';
+import { timeCurrentIso8601 } from '../utils';
 
 const Event = ({ event }) => {
   const { dispatch } = useContext(AppContext);
@@ -13,6 +14,12 @@ const Event = ({ event }) => {
       dispatch({
         type: DELETE_EVENT,
         id,
+      });
+
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: `イベント（id:${id}）を削除しました。`,
+        operatedAt: timeCurrentIso8601,
       });
     }
   };
--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,6 +1,12 @@
 import React, { useContext, useState } from 'react';
 import AppContext from '../contexts/AppContext';
-import { CREATE_EVENT, DELETE_ALL_EVENTS } from '../actions';
+import {
+  CREATE_EVENT,
+  DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  DELETE_ALL_OPERATION_LOGS,
+} from '../actions';
+import { timeCurrentIso8601 } from '../utils';
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext);
@@ -14,6 +20,13 @@ const EventForm = () => {
       title,
       body,
     });
+
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: 'イベントを作成しました。',
+      operatedAt: timeCurrentIso8601,
+    });
+
     setTitle('');
     setBody('');
   };
@@ -26,6 +39,12 @@ const EventForm = () => {
     if (result) {
       dispatch({
         type: DELETE_ALL_EVENTS,
+      });
+
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: '全てのイベントを削除しました。',
+        operatedAt: timeCurrentIso8601,
       });
     }
   };
